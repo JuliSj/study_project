d52a931e4533f18df776386aa763931ac6913bf8
JuliSj <senioritaj@gmail.com>
2023-12-13 01:24:32 +0500

    12.12.2023

--- News/users/forms.py
+++ News/users/forms.py
@@ -1,6 +1,26 @@
 from django import forms
 from .validators import russian_email
 from django.core.validators import MinLengthValidator
+from django.forms import TextInput, EmailInput, FileInput, Select
+from .models import Account
+
+class AccountUpdateForm(forms.Form):
+    class Meta:
+        model = Account
+        fields = ['phone', 'address', 'vk', 'instagram', 'telegram', 'account_image']
+        widgets = {'phone': TextInput({'class': 'textinput form-control',
+                                       'placeholder': 'phone number'}),
+                   'address': TextInput({'class': 'textinput form-control',
+                                         'placeholder': 'address'}),
+                   'vk': TextInput({'class': 'textinput form-control',
+                                      'placeholder': 'vk'}),
+                   'instagram': TextInput({'class': 'textinput form-control',
+                                         'placeholder': 'instagram'}),
+                   'telegram': TextInput({'class': 'textinput form-control',
+                                           'placeholder': 'telegram'}),
+                   'account_image': FileInput({'class': 'form-control',
+                                       'placeholder': 'image'})
+                   }
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100,
