9f0513a67b4a7d9fb5057b36a5ffb09584611a3f
JuliSj <senioritaj@gmail.com>
2023-12-24 13:48:14 +0500

    -

--- News/news1/models.py
+++ News/news1/models.py
@@ -60,7 +60,7 @@ class Article(models.Model):
 class Image(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     title = models.CharField(max_length=50, blank=True)
-    image = models.ImageField(upload_to='article_images/')
+    image = models.ImageField(upload_to='article_images/', null=True, blank=True)
 
     def __str__(self):
         return self.title
